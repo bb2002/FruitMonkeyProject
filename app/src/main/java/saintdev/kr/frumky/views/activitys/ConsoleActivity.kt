58d0ae3bc336190652ebfff3d26c1734f13f834b
Ballbot <5252bb@daum.net>
2018-10-27 01:23:04 +0900

    final commit 2

--- app/src/main/java/saintdev/kr/frumky/views/activitys/ConsoleActivity.kt
+++ app/src/main/java/saintdev/kr/frumky/views/activitys/ConsoleActivity.kt
@@ -13,107 +13,99 @@ import android.R.attr.data
 import android.R.attr.data
 import android.content.CursorLoader
 import android.media.MediaPlayer
+import android.util.Log
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import saintdev.kr.frumky.libs.bluetooth.BlueConnManager
 import saintdev.kr.frumky.libs.bluetooth.BlueSocket
 import java.io.File
 import java.lang.Exception
+import com.jaiselrahman.filepicker.activity.FilePickerActivity
+import com.jaiselrahman.filepicker.config.Configurations
+import com.jaiselrahman.filepicker.model.MediaFile
 
 
 class ConsoleActivity : AppCompatActivity(), BlueSocket.OnBlueSocketListener {
-    private lateinit var player: MediaPlayer
-    private val audioSet: HashMap<Int, Uri> = hashMapOf()
+    private lateinit var player: MediaPlayer            // 미디어 플레이어
+    private val playerQueue = arrayListOf<File>()       // 플레이 큐
+    private lateinit var adapter: ArrayAdapter<String>
+    private var indexPointer = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_console)
-
-        audio_select_1.setOnClickListener { onButtonClicked(it) }
-        audio_select_2.setOnClickListener { onButtonClicked(it) }
-        audio_select_3.setOnClickListener { onButtonClicked(it) }
-        audio_select_4.setOnClickListener { onButtonClicked(it) }
-        audio_select_5.setOnClickListener { onButtonClicked(it) }
-        audio_select_6.setOnClickListener { onButtonClicked(it) }
-
         BlueConnManager.getSocket().setReceiveListener(this)
 
         this.player = MediaPlayer()
-    }
+        this.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1)
+        play_quete.adapter = this.adapter
 
-    private fun onButtonClicked(view: View) {
-        val idx = when(view.id) {
-            R.id.audio_select_1 -> 0x1
-            R.id.audio_select_2 -> 0x2
-            R.id.audio_select_3 -> 0x3
-            R.id.audio_select_4 -> 0x4
-            R.id.audio_select_5 -> 0x5
-            R.id.audio_select_6 -> 0x6
-            else -> 0x1
-        }
-
-        val intent: Intent
-        intent = Intent()
-        intent.action = Intent.ACTION_GET_CONTENT
-        intent.type = "audio/mpeg"
-        startActivityForResult(Intent.createChooser(intent, "Select audio"), idx)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
-
-        if(resultCode == Activity.RESULT_OK) {
-            super.onActivityResult(requestCode, resultCode, data)
-            val uri = data.data
-
-            when(requestCode) {
-                0x1 -> {
-                    audio_select_1.text = uri.encodedPath
-                    audioSet[0] = uri
-                }
-                0x2 -> {
-                    audio_select_2.text = uri.encodedPath
-                    audioSet[1] = uri
-                }
-                0x3 -> {
-                    audio_select_3.text = uri.encodedPath
-                    audioSet[2] = uri
-                }
-                0x4 -> {
-                    audio_select_4.text = uri.encodedPath
-                    audioSet[3] = uri
-                }
-                0x5 -> {
-                    audio_select_5.text = uri.encodedPath
-                    audioSet[4] = uri
-                }
-                0x6 -> {
-                    audio_select_6.text = uri.encodedPath
-                    audioSet[5] = uri
-                }
-            }
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        player.release()
-        finish()
+        control_stop.setOnClickListener { onClickStop() }
+        control_clear.setOnClickListener { onAddMusic() }
     }
 
     override fun onReceive(msg: String?) {
-        runOnUiThread(Runnable {
+        runOnUiThread {
             if(msg != null) {
                 player.stop()
                 player.reset()
 
-                try {
-                    val idx = msg.toInt()
-                    player.setDataSource(this@ConsoleActivity, audioSet[idx-1])
+                if(msg == "1" && playerQueue.isNotEmpty()) {
+                    // 포인터 조절
+                    ++indexPointer
+                    if(playerQueue.size == indexPointer) indexPointer = 0
+
+                    val file = playerQueue[indexPointer]
+                    player.setDataSource(file.path)
                     player.prepare()
                     player.start()
-                } catch(ex: Exception) {
-                    Toast.makeText(this@ConsoleActivity, "No Int exception.", Toast.LENGTH_SHORT).show()
+
+                    control_now.text = file.name
                 }
             }
-        })
+        }
+    }
+
+    private fun onClickStop() {
+        this.adapter.clear()
+        this.playerQueue.clear()
+        control_now.text = ""
+        this.player.stop()
+        this.player.reset()
+        this.adapter.notifyDataSetChanged()
+    }
+
+    val FILE_SELECTOR = 0x1
+    private fun onAddMusic() {
+        val intent = Intent(this, FilePickerActivity::class.java)
+        intent.putExtra(FilePickerActivity.CONFIGS, Configurations.Builder()
+                .setCheckPermission(true)
+                .setShowAudios(true)
+                .setShowImages(false)
+                .setMaxSelection(10)
+                .setSkipZeroSizeFiles(true)
+                .build())
+        startActivityForResult(intent, FILE_SELECTOR)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == FILE_SELECTOR && resultCode == Activity.RESULT_OK && data != null) {
+            val files = data.getParcelableArrayListExtra<MediaFile>(FilePickerActivity.MEDIA_FILES)
+            files.forEach {
+                playerQueue.add(File(it.path))      // 큐에 포함한다.
+            }
+
+            if(!files.isEmpty()) {
+                empty_queue.visibility = View.GONE
+                play_quete.visibility = View.VISIBLE
+            }
+
+            // set adapter
+            playerQueue.forEach {
+                this.adapter.add(it.name)
+            }
+            this.adapter.notifyDataSetChanged()
+        }
     }
 }
